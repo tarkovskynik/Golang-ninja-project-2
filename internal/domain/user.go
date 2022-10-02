9f5d7c69ef3b9dc6060895b6cce0046384398bf8
cclock <nikolay4min99@gmail.com>
2022-10-02 23:45:58 +0400

    Added user entity and operations

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -5,7 +5,8 @@ import (
 )
 
 type User struct {
-	Email     string    `json:"email,omitempty" bson:"email" example:"bg@example.com"` // RFC 5322 address
+	// Email is unique value in DB
+	Email     string    `json:"email,omitempty" bson:"email" example:"bg@example.com"`
 	Password  string    `json:"password,omitempty" bson:"password" example:"Bill"`
 	Token     string    `json:"-" bson:"token"`
 	CreatedAt time.Time `json:"-" bson:"created_at"`
