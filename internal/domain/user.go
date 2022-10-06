cb8f1a1caf0e497c73b2045e1d02af37b0185521
cclock <nikolay4min99@gmail.com>
2022-10-06 09:39:51 +0300

    add Id for user

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -5,6 +5,7 @@ import (
 )
 
 type User struct {
+	Id uint64 `json:"id" bson:"_id,omitempty"`
 	// Email is unique value in DB
 	Email     string    `json:"email,omitempty" bson:"email" example:"bg@example.com"`
 	Password  string    `json:"password,omitempty" bson:"password" example:"Bill"`
